5f0c57b4c5e86794c535b04b5089a10c99f60b50
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2025-02-10 18:01:05 -0500

    fix: Remove count

--- main.tf
+++ main.tf
@@ -20,8 +20,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-  count = 2
+  instance_type = var.instance_type  
 
   tags = {
     Name = var.instance_name
